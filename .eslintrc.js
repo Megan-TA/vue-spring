d24813410025e80dc08863ef166eb095e5e8cb44
chen_huang <chen_huang@Ctrip.com>
2017-06-27 11:55:49 +0800

    修改eslint规范和sublimetmpl vue模板

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,9 +18,15 @@ module.exports = {
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'semi': ['error', 'always'],
+    'semi': [2, 'always'],
     'no-tabs': 0,
     'indent': 0,
-    'space-before-function-paren': 0
+    'space-before-function-paren': 0,
+    // 最大空行为3
+    'no-multiple-empty-lines': [1, { "max": 3 }],
+    // 块语句内行首行尾是否要空行
+    "padded-blocks": 0,
+    // 不以新行开始的块{前面必须要有1个空格
+    "space-before-blocks": [1, "always"],
   }
 }
---
+++ src/components/footer/footer.vue
@@ -0,0 +1,38 @@
+<template>
+
+
+
+
+</template>
+
+
+
+
+<script type="text/ecmascript-6">
+
+    props: {
+
+    },
+
+    data(){
+        return {
+
+        }
+    },
+
+    methods: {
+
+    }
+
+</script>
+
+
+
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+
+
+
+
+</style>
