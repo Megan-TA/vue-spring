0d8ce69e40e3df22067b5f7a21576945da09aec8
chen_huang <chen_huang@Ctrip.com>
2017-12-19 13:54:25 +0800

    登录注册样式

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -2,41 +2,50 @@
  * @Author: chen_huang 
  * @Date: 2017-08-06 11:26:50 
  * @Last Modified by: chen_huang
- * @Last Modified time: 2017-11-20 20:10:18
+ * @Last Modified time: 2017-12-19 12:00:35
  */
 <template>
     <div class="login">
-        <el-form 
-            label-width="80px" 
-            :label-position="labelPosition">
-            <el-form-item label="手机号">
-                <el-input 
-                    v-model="userPhone" 
-                    placeholder="请输入手机号" 
-                    name="userPhone"
-                    v-validate="'required|mobile|digits:11'"></el-input>
-                <span v-show="errors.has('userPhone')" class="el-form-item__error">{{ errors.first('userPhone') }}</span>
-            </el-form-item>
-            <el-form-item label="密码">
-                <el-input 
-                    type="password" 
-                    v-model="userPassword" 
-                    placeholder="请输入密码" 
-                    name="userPassword"
-                    v-validate="'required|min:6|max:20|alpha_num'"></el-input>
-                <span v-show="errors.has('userPassword')" class="el-form-item__error">{{ errors.first('userPassword') }}</span>
-            </el-form-item>
-            <el-row>
-                <el-col :span="5" :offset="15">
-                    <el-button type="primary" @click="validateForm">登录</el-button>
-                </el-col>
-                <el-col :span="4" class="l-h-36">
-                    <el-button size="mini">
-                        <router-link to='/register'>注册</router-link>
-                    </el-button>
-                </el-col>
-            </el-row>
-        </el-form>
+        <div class="login__content">
+            <el-form 
+                label-width="80px" 
+                :label-position="labelPosition"
+                class='login__form'>
+                <el-form-item label="手机号">
+                    <el-input 
+                        v-model="userPhone" 
+                        placeholder="请输入手机号" 
+                        name="userPhone"
+                        prefix-icon='icon-user'
+                        v-validate="'required|mobile|digits:11'"></el-input>
+                    <span v-show="errors.has('userPhone')" class="el-form-item__error">{{ errors.first('userPhone') }}</span>
+                </el-form-item>
+                <el-form-item label="密码">
+                    <el-input 
+                        type="password" 
+                        v-model="userPassword" 
+                        placeholder="请输入密码" 
+                        prefix-icon='icon-password'
+                        name="userPassword"
+                        v-validate="'required|min:6|max:20|alpha_num'"></el-input>
+                    <span v-show="errors.has('userPassword')" class="el-form-item__error">{{ errors.first('userPassword') }}</span>
+                </el-form-item>
+                <el-form-item class='login--mb0'>
+                    <el-button 
+                        type="primary" 
+                        @click="validateForm"
+                        class='login__loginBtn'>登录</el-button>
+                    <p class='login__hintBox'>
+                        <span class="login--hintText">
+                            <router-link to='/register' class='login__router--hover'>忘记密码</router-link>
+                        </span>
+                        <span class="login--hintText">
+                            <router-link to='/register' class='login__router--hover'>立即注册</router-link>
+                        </span>
+                    </p>
+                </el-form-item>
+            </el-form>
+        </div>
     </div>
 </template>
 <script>
@@ -115,9 +124,32 @@ export default {
 </script>
 <style lang="stylus">
     .login
+        position absolute
+        width 100%
+        top 50%
+        margin-top -300px
+    .login__content
+        width 100%
+        height 600px
+        display flex
+        justify-content center
+        align-items center
+        background #F56C6C
+    .login__form
         width 300px
-        margin 0 auto
-
-    .l-h-36
-        line-height 36px
+        padding 20px 80px 20px 20px
+        background #fff
+        border-radius 5px
+    .login__loginBtn
+        width 220px
+    .login__hintBox
+        text-align right 
+    .login--hintText
+        margin-left 10px
+    .login__router--hover
+        &:hover
+            color red
+    .login--mb0
+        margin-bottom 0!important
+    
 </style>
--- src/components/register/register.vue
+++ src/components/register/register.vue
@@ -2,47 +2,55 @@
  * @Author: chen_huang 
  * @Date: 2017-08-05 18:04:04 
  * @Last Modified by: chen_huang
- * @Last Modified time: 2017-12-08 15:17:51
+ * @Last Modified time: 2017-12-19 12:22:46
  */
 <template>
     <div class="register">
-        <el-form 
-            ref="ruleForm" 
-            :rules="rules"
-            :model='ruleForm'
-            label-width="80px"
-            status-icon>
-            <el-form-item label="昵称" prop='userName' class="is-required">
-                <el-input 
-                    v-model="ruleForm.userName" 
-                    name="userName"
-                    placeholder="请输入唯一的昵称"></el-input>
-            </el-form-item>
-            <el-form-item label="手机号" prop='userPhone' class="is-required">
-                <el-input
-                    v-model.number="ruleForm.userPhone" 
-                    name="userPhone"
-                    placeholder="请输入手机号"></el-input>
-            </el-form-item>
-            <el-form-item label="密码" prop='userPassword' class="is-required">
-                <el-input
-                    type="password"
-                    v-model="ruleForm.userPassword"
-                    placeholder="请输入密码">
-                </el-input>
-            </el-form-item>
-            <el-form-item label="确认密码" prop='userConfirmPasswod' class="is-required">
-                <el-input
-                    type="password"
-                    v-model="ruleForm.userConfirmPasswod"
-                    placeholder="请输入确认密码">
-                </el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click='validateForm'>立即注册</el-button>
-                <el-button @click='reset'>重置</el-button>
-            </el-form-item>
-        </el-form>
+        <div class="register__content">
+            <el-form 
+                ref="ruleForm" 
+                :rules="rules"
+                :model='ruleForm'
+                label-width="80px"
+                class='register__form'
+                status-icon>
+                <el-form-item label="昵称" prop='userName' class="is-required">
+                    <el-input 
+                        v-model="ruleForm.userName" 
+                        name="userName"
+                        placeholder="请输入唯一的昵称"></el-input>
+                </el-form-item>
+                <el-form-item label="手机号" prop='userPhone' class="is-required">
+                    <el-input
+                        v-model.number="ruleForm.userPhone" 
+                        name="userPhone"
+                        placeholder="请输入手机号"></el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop='userPassword' class="is-required">
+                    <el-input
+                        type="password"
+                        v-model="ruleForm.userPassword"
+                        placeholder="请输入密码">
+                    </el-input>
+                </el-form-item>
+                <el-form-item label="确认密码" prop='userConfirmPasswod' class="is-required">
+                    <el-input
+                        type="password"
+                        v-model="ruleForm.userConfirmPasswod"
+                        placeholder="请输入确认密码">
+                    </el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click='validateForm'>立即注册</el-button>
+                    <el-button @click='reset'>重置</el-button>
+                </el-form-item>
+                <p class='register--loginText'>
+                    已有账号？<router-link to='/login' class='register__router--hover'>立即登录</router-link>
+                </p>
+            </el-form>
+            
+            
+        </div>
     </div> 
 </template>
 <script>
@@ -180,8 +188,28 @@ export default {
 </script>
 <style lang="stylus">
     .register
-        width 500px
-        margin 0 auto
-
-
+        position absolute
+        width 100%
+        top 50%
+        margin-top -300px
+    .register__content
+        width 100%
+        height 600px
+        display flex
+        justify-content center
+        align-items center
+        background rgb(193, 34, 56)
+    .register__form
+        width 300px
+        padding 20px
+        background #fff
+        border-radius 5px
+    .register__form--marbom0
+        margin-bottom 0!important
+    .register--loginText
+        text-align right 
+    .register__router--hover
+        &:hover
+            color red
+        
 </style>
